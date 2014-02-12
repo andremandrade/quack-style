879ac0827299326a1ece7c87db7dc42e629ac67c
jaderxnet <jaderxnet@gmail.com>
2014-02-12 10:59:07 -0300

    Commit do projeto inicial
    Depois de muita luta. Ai vai Menu e Fase 01

---
+++ projetoUnity/Assets/Scripts/GUI/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public int boxMenuWidth = Screen.width*4/5;
+	public int boxMenuHeight = Screen.height*3/5;
+	public int buttonsWidth = 100;
+	public int buttonsHeight = 20;
+	public int buttonsSpace = 40;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+		GUI.BeginGroup (new Rect(Screen.width/2 - boxMenuWidth/2, Screen.height/2 - boxMenuHeight/2,
+		                         boxMenuWidth, boxMenuHeight),"");
+		bool teste1 = GUI.Button(new Rect(boxMenuWidth/2 - buttonsWidth/2, boxMenuHeight/2 - buttonsHeight/2
+		                                  , buttonsWidth, buttonsHeight), "Fase 01");
+		if(teste1){
+			Application.LoadLevel("fase01");
+		}
+		GUI.Button(new Rect(boxMenuWidth/2 - buttonsWidth/2, boxMenuHeight/2 - buttonsHeight/2 
+		                                  + 1 * buttonsSpace, buttonsWidth, buttonsHeight), "Fase 02");
+		GUI.Button(new Rect(boxMenuWidth/2 - buttonsWidth/2, boxMenuHeight/2 - buttonsHeight/2 
+		                                  + 2 * buttonsSpace, buttonsWidth, buttonsHeight), "Fase 03");
+		bool testeSair =GUI.Button(new Rect(boxMenuWidth/2 - buttonsWidth/2, boxMenuHeight/2 - buttonsHeight/2 
+		                    + 3 * buttonsSpace, buttonsWidth, buttonsHeight), "Sair");
+		if(testeSair){
+			Application.Quit();
+		}
+		GUI.EndGroup();
+	}
+}
---
+++ projetoUnity/Assets/Scripts/Player/MovimentoPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimentoPlayer : MonoBehaviour {
+	
+	public float aceleracao = 0.9f;
+	public float desAceleracao = 0.2f;
+	public float gravidade = 0.9f;
+	public float forcaPulo = 10f;
+	public float maxVelocidadeX = 3;
+	
+	private CharacterController jogador;
+	private float velocidadeX;
+	private float velocidadeY;
+	
+	void Start () {
+		jogador = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		if(Input.GetAxis("Horizontal") > 0){
+			velocidadeX += aceleracao;
+		}
+		if(Input.GetAxis("Horizontal") < 0){
+			velocidadeX -= aceleracao;
+		}
+		if(Input.GetButton("Jump") && jogador.isGrounded){
+			velocidadeY = forcaPulo;
+		}
+		
+		velocidadeX = Mathf.MoveTowards (velocidadeX, 0, desAceleracao);
+		velocidadeX = Mathf.Clamp(velocidadeX, -maxVelocidadeX, maxVelocidadeX);
+		
+		if(!jogador.isGrounded){
+			velocidadeY -= gravidade;
+		}
+		
+		jogador.Move(new Vector3(velocidadeX * Time.deltaTime,velocidadeY * Time.deltaTime,0));
+		
+	}
+}
