3cca726674e1826fee8d47f9bb185aa730023dc1
jaderxnet <jaderxnet@gmail.com>
2014-03-13 01:04:33 -0300

    Pato com comandos funcionando

--- projetoUnity/Assets/Scripts/Player/MovimentoPlayer.cs
+++ projetoUnity/Assets/Scripts/Player/MovimentoPlayer.cs
@@ -8,46 +8,69 @@ public class MovimentoPlayer : MonoBehaviour {
 	public float gravidade = 0.9f;
 	public float forcaPulo = 15f;
 	public float maxVelocidadeX = 3;
-	public float pontoAguaY = 1;	
+	public float pontoAguaY = 0.5f;	
 	public float forcaAgua = 2f;
 
 	private CharacterController jogador;
 	private float velocidadeX;
 	private float velocidadeY;
+	private float anguloX;
+	private float anguloY;
+
 
 	
 	void Start () {
 		jogador = GetComponent<CharacterController>();
 	}
+
+	private Vector3 moveDirection = Vector3.zero;
 	
 	void Update () {
 		bool emBaixoDaAgua = jogador.transform.position.y < pontoAguaY;
+		bool naSuperficie = jogador.transform.position.y == pontoAguaY;
+
+		anguloX = 90;
 
 		if(Input.GetAxis("Horizontal") > 0){
 			velocidadeX += aceleracao;
+			anguloY = 90;
 		}
 		if(Input.GetAxis("Horizontal") < 0){
 			velocidadeX -= aceleracao;
+			anguloY = 270;
 		}
 		if(Input.GetButton("Jump") && (jogador.isGrounded || emBaixoDaAgua)){
 			velocidadeY = forcaPulo;
-			animation.Stop();
-			animation.Play("jump");
 		}
 		
 		velocidadeX = Mathf.MoveTowards (velocidadeX, 0, desAceleracao);
 		velocidadeX = Mathf.Clamp(velocidadeX, -maxVelocidadeX, maxVelocidadeX);
 
-		if(velocidadeY < gravidade && emBaixoDaAgua){
+		if(velocidadeY <= gravidade && emBaixoDaAgua){
 			velocidadeY += forcaAgua;
+			if(naSuperficie){
+				velocidadeY = 0;
+			}
 		}
 		
-		if(!jogador.isGrounded){
-			 velocidadeY -= gravidade;
+		if (!jogador.isGrounded) {
+			velocidadeY -= gravidade;
+			if (velocidadeY > 0) {
+				anguloX = -30;
+			}
 
+			if (velocidadeY < 0) {
+				anguloX = 30;
+			}
+		} 
+
+		if(jogador.isGrounded || naSuperficie){
+			anguloX = 0;
 		}
-		
-		jogador.Move(new Vector3(velocidadeX * Time.deltaTime,velocidadeY * Time.deltaTime,0));
-		
+
+		moveDirection.Set (velocidadeX * Time.deltaTime, velocidadeY * Time.deltaTime, 0);
+
+		jogador.transform.rotation = Quaternion.Euler( new Vector3(anguloX, anguloY,0));
+		jogador.Move(moveDirection );
 	}
 }
